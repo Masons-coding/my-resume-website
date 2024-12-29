2a089c9812daa27e5fb86da9f4f99f1ea3030e85
Mason Clarke <maclarkegdci@gmail.com>
2024-12-29 00:17:33 -0500

    Changed footer
    Added GitHub link to footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,12 +1,22 @@
 import './Footer.scss';
 
+import githubLogo from "../../assets/images/github-icon.svg";
+
 const Footer = () => {
+
+function gitClicked() {
+    window.open('https://github.com/Masons-coding/my-resume-website');
+}
 
     return (
         <>
         <div className="footer-container">
             <h1 className="footer-message">Thank you!</h1>
-            <p className="footer-text">This was made using React, Javascript, HTML and SCSS</p>
+            <div className="footer-github-container">
+                <p className="footer-text">This was made using React, Javascript, HTML and SCSS</p>
+                <img onClick={gitClicked} className="github-logo" src={githubLogo} alt="Github Logo"/>
+            </div>
+            
         </div>
         </>
     );
